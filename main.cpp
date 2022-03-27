8f326ca9c248a79549cfbe0dfc8542b8b769f3b7
luckybsmith <vs1707@naver.com>
2022-03-28 01:18:21 +0900

    Hi im local1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ForGitHub
+//
+//  Created by Leesongeun on 2022/03/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
